7de046804dd1bf06cea5295cbd1d51d1f759a23f
root <ddphillyfan@gmail.com>
2020-05-20 08:30:56 -0400

    Able to update task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavBar from './components/NavBar';
-import ProjectBoard from './components/ProjectBoard';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import Navigation from "./components/Navigation";
+import ProjectBoard from "./components/ProjectBoard";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProjectTask from "./components/ProjectTask/AddProjectTask";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import store from "./store";
+import UpdateProjectTask from "./components/ProjectTask/UpdateProjectTask";
 
-function App() {
+class App extends Component {
+  render() {
     return (
-        <Provider store={store}>
-            <Router>
-                <div className="App">
-                    <NavBar />
-                    <Route exact path="/" component={ProjectBoard}/>
-                    <Route exact path="/addProjectTask" component={AddProjectTask}/>
-                </div>
-            </Router>
-        </Provider>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navigation />
+            <Route exact path="/" component={ProjectBoard} />
+
+            <Route exact path="/addProjectTask" component={AddProjectTask} />
+            <Route
+              exact
+              path="/updateProjectTask/:pt_id"
+              component={UpdateProjectTask}
+            />
+          </div>
+        </Router>
+      </Provider>
     );
+  }
 }
 
 export default App;
